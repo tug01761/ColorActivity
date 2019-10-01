2352abdc943a60784c973118546412bd231a2301
Mike Lauw <tug01761@temple.edu>
2019-10-01 12:21:38 -0400

    making arrays & calling the spinners

--- app/src/main/java/temple/edu/coloractivity/MainActivity.java
+++ app/src/main/java/temple/edu/coloractivity/MainActivity.java
@@ -3,12 +3,27 @@ package temple.edu.coloractivity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+
+    String[] colors={"Red", "Blue", "Yellow", "Purple", "Orange", "Gray"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner spinner = (Spinner) findViewById(R.id.mySpinner);
+        spinner.setOnItemClickListener(this);
+
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id)
+    {
+
     }
 }
